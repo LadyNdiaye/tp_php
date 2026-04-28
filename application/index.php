693b84c59b46a2300932d77b4efeae213ace11ee
LadyNdiaye <ndeyecoumbandiaye714@gmail.com>
2026-04-28 22:40:19 +0000

    les débuts

---
+++ application/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>TeamUp - Accueil</title>
+</head>
+<body>
+    <h1>Bienvenue sur le projet TeamUp </h1>
+    <p>La redirection a fonctionné avec succès.</p>
+    <p>Nous sommes le : <?php echo date('d/m/Y'); ?></p>
+</body>
+</html>
